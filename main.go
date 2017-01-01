5b10277ddce0c249d315aad55e86bfbeb1bb88a6
Andrew Arrow <oneone@gmail.com>
2017-01-01 02:52:15 +0000

    me

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import "time"
 import "strings"
 
 var UserState map[string]int = make(map[string]int)
+var Me string
 
 func handleRtm(rtm *slack.RTM) {
 
@@ -15,9 +16,9 @@ func handleRtm(rtm *slack.RTM) {
 		case msg := <-rtm.IncomingEvents:
 			switch ev := msg.Data.(type) {
 			case *slack.MessageEvent:
-				//fmt.Println(ev.Msg.Type, ev.Msg.Channel, ev.Msg.User, ev.Msg.Username, ev.Msg.BotID)
+				fmt.Println(ev.Msg.Type, ev.Msg.Channel, ev.Msg.User, ev.Msg.Text)
 
-				if strings.HasPrefix(ev.Msg.Channel, "D") {
+				if ev.Msg.User != Me && strings.HasPrefix(ev.Msg.Channel, "D") {
 					//name := ev.Msg.Text
 					//api := slack.New(os.Getenv("BOTADMIN"))
 					//api.CreateGroup(name)
@@ -25,19 +26,18 @@ func handleRtm(rtm *slack.RTM) {
 
 					from := ev.Msg.User
 
-					if UserState[from] == 0 {
+					switch UserState[from] {
+					case 0:
 						m := rtm.NewOutgoingMessage("Hello, let's start your proposal. What is first name?", ev.Msg.Channel)
 						rtm.SendMessage(m)
-						UserState[from] = 1
-					} else if UserState[from] == 1 {
+					case 1:
 						m := rtm.NewOutgoingMessage("What is last name?", ev.Msg.Channel)
 						rtm.SendMessage(m)
-						UserState[from] = 2
-					} else {
+					default:
 						m := rtm.NewOutgoingMessage("All done", ev.Msg.Channel)
 						rtm.SendMessage(m)
-						UserState[from] = 9
 					}
+					UserState[from] += 1
 				}
 
 				//name := ev.Msg.Channel
@@ -55,6 +55,13 @@ func handleRtm(rtm *slack.RTM) {
 func main() {
 	fmt.Println("listening for proposals...")
 	api := slack.New(os.Getenv("BOT"))
+	list, _ := api.GetUsers()
+	for _, u := range list {
+		if u.Name == "proposal" {
+			Me = u.ID
+			break
+		}
+	}
 
 	rtm := api.NewRTM()
 
