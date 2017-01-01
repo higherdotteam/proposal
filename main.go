8c8820dc47da040490f401ee7d05743087257da6
A Arrow <oneone@gmail.com>
2017-01-01 01:47:09 +0000

    m

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "github.com/nlopes/slack"
 import "os"
+import "time"
 
 func handleRtm(rtm *slack.RTM) {
 
@@ -11,7 +12,18 @@ func handleRtm(rtm *slack.RTM) {
 		case msg := <-rtm.IncomingEvents:
 			switch ev := msg.Data.(type) {
 			case *slack.MessageEvent:
-				fmt.Println(ev, ev.Msg)
+				/*
+					&{{message D3LAES7C5 U035LF6C1 wefwef 1483234485.000004 false [] [] <nil>  false     <nil>      [] <nil> false <nil>  0 T035N23CL []} <nil>} {message D3LAES7C5 U035LF6C1 wefwef 1483234485.000004 false [] [] <nil>  false     <nil>      [] <nil> false <nil>  0 T035N23CL []}
+
+				*/
+				//fmt.Println(ev, ev.Msg)
+				name := ev.Msg.Text
+				m := rtm.NewOutgoingMessage("Okay making a new private channel for: "+name, ev.Msg.Channel)
+				rtm.SendMessage(m)
+				//api := slack.New(os.Getenv("BOT"))
+				//g, _ := api.CreateGroup("name")
+				//_, _, _ = api.InviteUserToGroup("name", "user")
+
 				//name := ev.Msg.Channel
 
 				//h["text"] = ev.Msg.Text
@@ -31,4 +43,8 @@ func main() {
 
 	go rtm.ManageConnection()
 	go handleRtm(rtm)
+
+	for {
+		time.Sleep(time.Second)
+	}
 }
